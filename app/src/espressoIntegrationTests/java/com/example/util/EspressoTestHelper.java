9d69bc048ceaa429ec419d48132a540c8850c1e7
Sakis Kaliakoudas <sakis@lifesign.io>
2017-04-14 19:46:29 +0100

    [No issue]: Disabling animations before epsresso tests.

--- app/src/espressoIntegrationTests/java/com/example/util/EspressoTestHelper.java
+++ app/src/espressoIntegrationTests/java/com/example/util/EspressoTestHelper.java
@@ -2,6 +2,10 @@ package com.example.util;
 
 import android.content.Intent;
 import android.support.annotation.IdRes;
+import android.support.test.InstrumentationRegistry;
+import com.metova.cappuccino.animations.SystemAnimations;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -11,6 +15,16 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.not;
 
 public class EspressoTestHelper {
+
+    @BeforeClass
+    public static void beforeClass() {
+        SystemAnimations.disableAll(InstrumentationRegistry.getContext());
+    }
+
+    @AfterClass
+    public static void enableAnimations() {
+        SystemAnimations.enableAll(InstrumentationRegistry.getContext());
+    }
 
     protected static final Intent NO_INTENT = null;
 
