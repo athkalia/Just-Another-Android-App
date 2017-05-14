9fe8518f04ccfea9beb9ba9eb08ba35f1cdc95f2
Sakis Kaliakoudas <sakis@lifesign.io>
2017-05-14 20:32:40 +0100

    Fixed some test related annotations. Upped android gradle plugin version and gradle

--- app/src/main/java/com/example/util/testing/ForTestingPurposes.java
+++ app/src/main/java/com/example/util/testing/ForTestingPurposes.java
@@ -5,6 +5,7 @@ import java.lang.annotation.RetentionPolicy;
 
 /**
  * An annotation that marks elements that only exist for testing purposes.
+ * Just a marker interface essentially.
  */
 @Retention(RetentionPolicy.SOURCE)
 public @interface ForTestingPurposes {
