d055a0a3eb73d49c30426832f333a145fe4e8dee
Sakis Kaliakoudas <sakis.kaliakoudas@babylonhealth.com>
2018-01-04 01:49:57 +0000

    Upgraded gradle plugin to 3.0 (#96)
    * Upgraded gradle plugin to 3.0
    Upgraded a couple of dependencies
    
    * Upgraded gradle plugin to 3.0: Updated readme file
    
    * Upgraded gradle plugin to 3.0:
    Fixed lint issues.
    There's one false positive that was suppressed
    
    * Upgraded gradle plugin to 3.0:
    Upgraded robolectric to make it work with the new plugin.
    Made tests run before static analysis tools in CI so that findbugs can find compiled classes when it tries to run (it used to compile them automatically but that doesn't seem to be the case any more)
    
    * Upgraded gradle plugin to 3.0:
    Now creating lint jar before running lint. This is also because findbugs needs to operate on the classes
    
    * Upgraded gradle plugin to 3.0:
    Changed quiet periods a bit for pull requests, lowered them as 10 mins was too much.
    Upgraded pom.xml to 2017.2 version so that we can benefit from editable Teamcity UI
    
    * Upgraded gradle plugin to 3.0
    Changed java plugin to java-library plugin // java plugin that knows about `api` and `implementation
    
    * Upgraded gradle plugin to 3.0
    Added a missing espresso dependency.
    
    * Upgraded gradle plugin to 3.0
    Updated APK directories in pipeline files

--- app/src/unitTests/java/com/example/util/PreconfiguredRobolectricTestRunner.java
+++ app/src/unitTests/java/com/example/util/PreconfiguredRobolectricTestRunner.java
@@ -1,13 +1,12 @@
 package com.example.util;
 
-import com.example.BuildConfig;
 import org.junit.runners.model.InitializationError;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 /**
- * A {@link RobolectricTestRunner} that allows us to set robolectric configuration in one place instead of setting it in each test class
- * via {@link Config}.
+ * A {@link RobolectricTestRunner} that allows us to set robolectric configuration in one place (which is usually
+ * enough).
  */
 public class PreconfiguredRobolectricTestRunner extends RobolectricTestRunner {
 
@@ -21,7 +20,6 @@ public class PreconfiguredRobolectricTestRunner extends RobolectricTestRunner {
     protected Config buildGlobalConfig() {
         return new Config.Builder()
                 .setSdk(SDK_API_LEVEL_TO_EMULATE)
-                .setConstants(BuildConfig.class)
                 .build();
     }
 
